7b7c846159156a1092f67165a925c1bef3415840
Annapoorani Bala <Arunkumar>
2020-08-13 11:53:01 +0530

    Changed the script based on pylint suggestions

--- utils/spoiler.py
+++ utils/spoiler.py
@@ -2,26 +2,26 @@
 Adding the decorator to change the return value for the method
 
 """
-import json
 import random
 
 def get_random_status():
     "Get the random status"
     status_list = ["301", "500", "502"]
+
     return random.choice(status_list)
 
 
 def change_json_value(result):
     "Change the json value"
     result.json.clear()
-    result.json['Name']='Null'
+    result.json['Name'] = 'Null'
 
     return result
 
 
 def change_headers(result):
     "Change the headers"
-    result.headers._list = [('null','value')]
+    result.headers._list = [('null', 'value')]
 
     return result
 
@@ -32,22 +32,16 @@ class Spoiler():
         "Use this decorator to vary your funtion return value"
         def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
-            if random.uniform(0,1) < 0.9:
-                print("status")
+            if random.uniform(0, 1) < 0.9:
                 result.status = get_random_status()
-            if random.uniform(0,1) < 0.1:
-                print("hi")
+            if random.uniform(0, 1) < 0.1:
                 result = change_json_value(result)
-            if random.uniform(0,1) < 0.01:
-                print("headers")
+            if random.uniform(0, 1) < 0.01:
                 result = change_headers(result)
 
             return result
 
         return wrapper
-
-
-
 
 
     vary_me = staticmethod(vary_me)
